04a661d0872950c2bc6ce22b0f29660331e77b70
Bruno <brunobarboza3103@gmail.com>
2021-05-05 15:29:18 -0300

    Tela 2

--- flutter_application_1/lib/second_page.dart
+++ flutter_application_1/lib/second_page.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/third_page.dart';
 
 class SecondPage extends StatelessWidget {
+  bool _sel = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("RESULTADOS DA PESQUISA"),
-        ),
-        body: Container(
-            decoration: BoxDecoration(color: Colors.green[100]),
-            child: Center(
-                child: RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ThirdPage()));
-              },
-              child: Text(
-                "Aplicar Filtro",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black87,
+      appBar: AppBar(
+        title: Text("ESCOLHA A TEMÁTICA"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.all(10.0),
+                  alignment: Alignment.topLeft,
+                  width: 300,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    border: Border.all(),
+                  ),
+                  child: Text("Atividades Plugadas",
+                      style: TextStyle(fontSize: 25)),
                 ),
-              ),
-            ))));
+                Checkbox(
+                  onChanged: (bool resp) {
+                    setState(() {
+                      _sel = resp;
+                    });
+                  },
+                  value: _sel,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
+
+  void setState(Null Function() param0) {}
 }
