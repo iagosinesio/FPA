7f73f51e090adf7485009d0334726c2087783322
Bruno <brunobarboza3103@gmail.com>
2021-05-22 15:09:13 -0300

    Tela 2

--- flutter_application_1/lib/second_page.dart
+++ flutter_application_1/lib/second_page.dart
@@ -1,52 +1,48 @@
 import 'package:flutter/material.dart';
 
-enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
-
 class SecondPage extends StatefulWidget {
   @override
   _SecondPageState createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
-  dynamic _selection = WhyFarther.harder;
+  String valueChoose;
+  List listItem = ["Atividades Plugadas", "Atividades Desplugadas"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ESCOLHA A TEMÁTICA"),
-      ),
-      body: Column(
-        children: [
-          PopupMenuButton<WhyFarther>(
-            child: ListTile(
-              title: Text(_selection.toString()),
-            ),
-            onSelected: (WhyFarther result) {
-              setState(() {
-                _selection = result;
-              });
-            },
-            itemBuilder: (BuildContext context) => <PopupMenuEntry<WhyFarther>>[
-              const PopupMenuItem<WhyFarther>(
-                value: WhyFarther.harder,
-                child: Text('Working a lot harder'),
-              ),
-              const PopupMenuItem<WhyFarther>(
-                value: WhyFarther.smarter,
-                child: Text('Being a lot smarter'),
-              ),
-              const PopupMenuItem<WhyFarther>(
-                value: WhyFarther.selfStarter,
-                child: Text('Being a self-starter'),
-              ),
-              const PopupMenuItem<WhyFarther>(
-                value: WhyFarther.tradingCharter,
-                child: Text('Placed in charge of trading charter'),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Escolha as Opções"),
+        ),
+        body: Center(
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1)),
+                child: DropdownButton(
+                  hint: Text("Seleciona a Temática"),
+                  dropdownColor: Colors.white,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 36,
+                  isExpanded: true,
+                  underline: SizedBox(),
+                  style: TextStyle(color: Colors.black, fontSize: 22),
+                  value: valueChoose,
+                  onChanged: (newValue) {
+                    setState(() {
+                      valueChoose = newValue;
+                    });
+                  },
+                  items: listItem.map((valueItem) {
+                    return DropdownMenuItem(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+                ),
+              )),
+        ));
   }
 }
