1c88d9b79cec2cdec2f02fd1186a5235115e42b4
Bruno <brunobarboza3103@gmail.com>
2021-06-01 14:41:29 -0300

    Atualização

--- flutter_application_1/lib/second_page.dart
+++ flutter_application_1/lib/second_page.dart
@@ -15,27 +15,47 @@ class _SecondPageState extends State<SecondPage> {
         title: Text("Escolha as Opções"),
       ),
       body: Container(
-        decoration: BoxDecoration(color: Colors.green[100]),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => ThirdPage()));
-                },
-                child: Text("Atividades Plugadas"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => FourtPage()));
-                },
-                child: Text("Atividades Desplugadas"),
-              ),
-            ],
-          ),
+        color: Colors.green[100],
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: [
+                Container(
+                  child: Text(
+                    "TEMÁTICA",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ThirdPage()));
+                      },
+                      child: Text("Atividades Plugadas"),
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => FourtPage()));
+                      },
+                      child: Text("Atividades Desplugadas"),
+                    ),
+                  ]),
+            )
+          ],
         ),
       ),
     );
