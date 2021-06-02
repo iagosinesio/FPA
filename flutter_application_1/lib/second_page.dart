330506f820ccdbab271762c1f343466196438276
Bruno <brunobarboza3103@gmail.com>
2021-06-02 15:30:35 -0300

    com

--- flutter_application_1/lib/second_page.dart
+++ flutter_application_1/lib/second_page.dart
@@ -16,45 +16,79 @@ class _SecondPageState extends State<SecondPage> {
       ),
       body: Container(
         color: Colors.green[100],
-        alignment: Alignment.center,
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Column(
-              children: [
-                Container(
-                  child: Text(
-                    "TEMÁTICA",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 5.0),
+              padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 5.0),
+              width: 400,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.green[50],
+                border: Border.all(),
+              ),
+              child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Center(
+                    child: Text(
+                      "TEMÁTICA",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )),
             ),
             Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    RaisedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ThirdPage()));
-                      },
-                      child: Text("Atividades Plugadas"),
-                    ),
-                    RaisedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => FourtPage()));
-                      },
-                      child: Text("Atividades Desplugadas"),
-                    ),
-                  ]),
-            )
+              child: Align(
+                alignment: Alignment.center,
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 300,
+                          height: 100,
+                          child: RaisedButton(
+                            color: Colors.blue[200],
+                            elevation: 4.0,
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => ThirdPage()));
+                            },
+                            child: Text(
+                              "Atividades Plugadas",
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 300,
+                          height: 100,
+                          child: RaisedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => FourtPage()));
+                            },
+                            child: Text(
+                              "Atividades Desplugadas",
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+            ),
           ],
         ),
       ),
