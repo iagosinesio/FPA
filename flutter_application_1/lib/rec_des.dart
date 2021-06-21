e0f6cb76dc9ef59d0f149c0cb916caff8b382dc1
Bruno <brunobarboza3103@gmail.com>
2021-06-21 20:31:34 -0300

    Atl

--- flutter_application_1/lib/rec_des.dart
+++ flutter_application_1/lib/rec_des.dart
@@ -1,16 +1,14 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class RecDes extends StatefulWidget {
-  @override
-  _RecDesState createState() => _RecDesState();
-}
-
-class _RecDesState extends State<RecDes> {
+class RecDes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("REC. DE PADRÕES"),
+        title: Text("Rec. de Padrões Plugado"),
       ),
       body: Container(
         child: Column(
@@ -31,7 +29,7 @@ class _RecDesState extends State<RecDes> {
                   alignment: Alignment.topCenter,
                   child: Center(
                     child: Text(
-                      "REC. DE PADRÕES DESPLUGADO",
+                      "REC. DE PADRÕES",
                       style: TextStyle(
                           color: Colors.black,
                           fontSize: 25,
@@ -39,6 +37,48 @@ class _RecDesState extends State<RecDes> {
                     ),
                   )),
             ),
+            Container(
+              width: 200,
+              height: 40,
+            ),
+            Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    InkWell(
+                        child: Text('Colorindo com Números',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'http://desplugada.ime.unicamp.br/atividade2/index.html')),
+                    InkWell(
+                        child: Text('Você pode Repetir?',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'http://desplugada.ime.unicamp.br/atividade3/index.html')),
+                    InkWell(
+                        child: Text('Criptografia para Jovens',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'http://desplugada.ime.unicamp.br/atividade19/index.html')),
+                    InkWell(
+                        child: Text('Mapa da Turma da Mônica',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Boneca de Papel',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Cupcakes',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
--- flutter_application_1/lib/todos_des.dart
+++ flutter_application_1/lib/todos_des.dart
@@ -1,16 +1,14 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class TodosDes extends StatefulWidget {
-  @override
-  _TodosDesState createState() => _TodosDesState();
-}
-
-class _TodosDesState extends State<TodosDes> {
+class TodosDes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TODOS"),
+        title: Text("Todos Desplugado"),
       ),
       body: Container(
         child: Column(
@@ -31,7 +29,7 @@ class _TodosDesState extends State<TodosDes> {
                   alignment: Alignment.topCenter,
                   child: Center(
                     child: Text(
-                      "TODOS DESPLUGADO",
+                      "TODOS OS COMPONEMTES",
                       style: TextStyle(
                           color: Colors.black,
                           fontSize: 25,
@@ -39,6 +37,52 @@ class _TodosDesState extends State<TodosDes> {
                     ),
                   )),
             ),
+            Container(
+              width: 200,
+              height: 40,
+            ),
+            Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    InkWell(
+                        child: Text('Seja mais Rápido!',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'http://desplugada.ime.unicamp.br/atividade8/index.html')),
+                    InkWell(
+                        child: Text('Tetris',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Algoritmo Musical',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Autômatos da Mônica',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Estacioamento Algorítmico',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://www.computacional.com.br/#atividades')),
+                    InkWell(
+                        child: Text('Pensamento Computacional',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch(
+                            'https://umlivroaberto.org/producao/atividades-de-pensamento-computacional/')),
+                    InkWell(
+                        child: Text('Sertão.Bit',
+                            style: TextStyle(color: Colors.blue, fontSize: 30)),
+                        onTap: () => launch('https://www.falecomrozelma.com/')),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
